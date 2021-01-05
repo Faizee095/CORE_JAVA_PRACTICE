81979781f4ce601cbef6f908e87ee5ff9e8e12a1
faizee <faiyazsiddique9@gmail.com>
2021-01-05 15:36:54 +0530

    OPA 11 DEC

---
+++ Solution1.java
@@ -0,0 +1,103 @@
+// OPA 11deC
+
+import java.util.Scanner;
+
+class Item{
+    int itemId;
+    String itemName;
+    String itemType;
+    double itemPrice;
+    Item(int itemId, String itemName, String itemType, double itemPrice){
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.itemType = itemType;
+        this.itemPrice = itemPrice;
+    }
+    int getItemId(){
+        return itemId;
+    }
+    String getItemName(){
+        return itemName;
+    }
+    String getItemType(){
+        return itemType;
+    }
+    double getItemPrice(){
+        return itemPrice;
+    }
+}
+
+public class Solution1{
+    public static void main(String[] args){
+        int itemId;
+        String itemName;
+        String itemType;
+        double itemPrice;
+        Item[] item = new Item[4];
+        Scanner s = new Scanner(System.in);
+        for(int i=0;i<item.length;i++){
+            itemId = s.nextInt();s.nextLine();
+            itemName = s.nextLine();
+            itemType = s.nextLine();
+            itemPrice = s.nextDouble();
+            item[i] = new Item(itemId, itemName, itemType, itemPrice);
+        }
+        s.nextLine();
+        String newItemType = s.nextLine();
+        String newItemName = s.nextLine();
+        s.close();
+
+        int avgItemPrice = findAvgItemPriceByTypes(item, newItemType);
+        Item[] items = searchItemByName(item, newItemName);
+
+        if(avgItemPrice > 0)
+            System.out.println(avgItemPrice);
+        else
+            System.out.println("There are no items with given type");
+
+        if(items == null)
+            System.out.println("There are no items with the given name");
+        else{
+            for(int i=0;i<items.length;i++){
+                System.out.println(items[i].getItemId());
+            }
+        }
+    }
+    public static int findAvgItemPriceByTypes(Item[] item, String itemType){
+        int avgPrice = 0, count = 0;
+        double sum = 0;
+        for(int i=0;i<item.length;i++){
+            if(itemType.equalsIgnoreCase(item[i].getItemType())) {
+                sum += item[i].getItemPrice();
+                count++;
+            }
+        }
+        avgPrice = (int)(sum/count);
+        return avgPrice;
+    }
+    public static Item[] searchItemByName(Item[] item, String itemName){
+        int count = 0;
+        for(int i=0;i<item.length;i++){
+            if(itemName.equalsIgnoreCase(item[i].getItemName()))
+                count++;
+        }
+        if(count == 0) return null;
+        Item[] items = new Item[count];
+        count = 0;
+        for(int i=0;i<item.length;i++){
+            if(itemName.equalsIgnoreCase(item[i].getItemName())){
+                items[count++] = item[i];
+            }
+        }
+        for(int i=0;i<items.length;i++){
+            for(int j=i+1;j<items.length;j++){
+                if(items[i].getItemId() >= items[j].getItemId()){
+                    Item temp = items[i];
+                    items[i] = items[j];
+                    items[j] = temp;
+                }
+            }
+        }
+        return items;
+    }
+}
