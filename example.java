7a2c404a3390db813e1239ed434c2b246ad473ab
unknown <faiyazsiddique9@gmail.com>
2021-06-14 17:31:13 +0530

    Checking

--- example.java
+++ example.java
@@ -1,26 +1,29 @@
-// this way we declare object and access it
-
-class Employee{
-  String name;
-  int salary;
-  String cname;
+// static variable
+class Emplooye{
+  private String name;
+  private int salary;
+  private static String cname;
+  public void input(String n,int s){
+    name=n;
+    salary=s;
+  }
+  public void cinput(String c){
+    cname=c;
+  }
+  void show(){
+    System.out.println(name+" "+salary+" "+cname);
+  }
 }
 
 
-public class example {
+public class Example {
   public static void main(String args[]){
-    Employee e1 =new Employee();
-    Employee e2 =new Employee();
-    e1.name="Robert";
-    e1.salary=23000;
-    e1.cname="GArbage";
-    e2.name="JArvis";
-    e2.salary=23040;
-    e2.cname="teapot";
-    System.out.println(e1.name+" "+e1.salary+" "+e1.cname);
-    System.out.println(e2.name+" "+e2.salary+" "+e2.cname);
+    Emplooye e1 =new Emplooye();
+    Emplooye e2 =new Emplooye();
+    e1.cinput("Gada Electronics");
+    e1.input("Bagha", 1200 );
+    e2.input("Jethalal", 120000);
+    e1.show();
+    e2.show();
   }
-  
-
-  
 }
